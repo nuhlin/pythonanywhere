e97eff8dae023660715faff7dcdbb2bc733a27c2
Ulin Nuha <nuha.ulin@gmail.com>
2025-08-07 05:09:19 +0000

    Connected to database

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,20 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://ulinnuha:ultra2003@ulinnuha.mysql.pythonanywhere-services.com/ulinnuha$comments".format(
+    username="the username from the 'Databases' tab",
+    password="the password you set on the 'Databases' tab",
+    hostname="the database host address from the 'Databases' tab",
+    databasename="the database name you chose, probably yourusername$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 comments = []
 @app.route("/", methods=["GET", "POST"])
 def index():
