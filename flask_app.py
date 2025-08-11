306695cd60251c5a8db47f1d09059288df5aef87
Ulin Nuha <nuha.ulin@gmail.com>
2025-08-11 06:28:07 +0000

    Added timestamp

--- flask_app.py
+++ flask_app.py
@@ -3,8 +3,9 @@
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import login_user, LoginManager, UserMixin, login_required, logout_user
+from flask_login import login_user, LoginManager, UserMixin, login_required, logout_user, current_user
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -45,6 +46,7 @@ all_users = {
 def load_user(user_id):
     return all_users.get(user_id)
 
+
 class Comment(db.Model):
 
     __tablename__ = "comments"
@@ -55,7 +57,10 @@ class Comment(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
+        return render_template("main_page.html", comments=Comment.query.all(), timestamp=datetime.now())
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('index'))
 
     comment = Comment(content=request.form["contents"])
     db.session.add(comment)
